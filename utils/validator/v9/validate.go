e9e7d4b5897626e4d878f1aa4ec864acc5bfd94f
huarong.zheng <rg@huarongzhengs-MacBook-Pro.local>
2019-08-15 18:08:52 +0800

    :bulb: fix v9 validate for gin

--- utils/validator/v9/validate.go
+++ utils/validator/v9/validate.go
@@ -1,6 +1,12 @@
 package v9
 
+import "github.com/gin-gonic/gin/binding"
+
 func ValidateStruct(obj interface{}) error {
 	var validate = defaultValidator{}
 	return validate.ValidateStruct(obj)
+}
+
+func init() {
+	binding.Validator = new(defaultValidator)
 }
